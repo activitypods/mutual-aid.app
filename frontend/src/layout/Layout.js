301ee5bf4724be0668d4d22aae75291b60b78745
srosset81 <srosset81@gmail.com>
2024-09-11 18:35:14 +0200

    Upgrade packages

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -7,10 +7,13 @@ import TopAppBar from './TopAppBar';
 import ScrollToTop from './ScrollToTop';
 
 const Layout = ({ children, title }) => {
-  const { identity } = useCheckAuthenticated();
-  if (!identity?.id) return null;
+  const { identity, isLoading } = useCheckAuthenticated();
+  if (isLoading) return null;
   return (
-    <BackgroundChecks>
+    <BackgroundChecks
+      clientId={process.env.REACT_APP_BACKEND_CLIENT_ID}
+      listeningTo={[identity?.webIdData.inbox, identity?.webIdData.outbox]}
+    >
       <ScrollToTop />
       <TopAppBar />
       <Box mt={{ xs: 3, sm: 5 }} mb={{ xs: 2, sm: 5 }}>
